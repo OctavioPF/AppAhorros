280d933f54f9af65ab778e4240f4a9bb4de79e05
Jose Octavio Palacio Flores <111895528+OctavioPF@users.noreply.github.com>
2024-07-02 16:11:18 -0600

    funcionalidad movimientos

--- ahorro-app/src/components/HelloWorld.vue
+++ ahorro-app/src/components/HelloWorld.vue
@@ -5,10 +5,23 @@
     </template>
 
     <template #resume>
-        <Resume></Resume>
+      <Resume
+        :total-label="'Ahorro total'"
+        :label="label"
+        :total-amount="500"
+        :amount="amount"
+      >
+        <template #graphic> grapic </template>
+        <template #action>
+          <Action/>
+        </template>
+      </Resume>
     </template>
     <template #movementsComponent>
-      <MovementsComponent></MovementsComponent>
+      <MovementsComponent 
+      :movements="movements">
+    
+    </MovementsComponent>
     </template>
   </LayoutComponent>
 </template>
@@ -17,7 +30,8 @@
 import LayoutComponent from "./LayoutComponent.vue";
 import HeaderComponent from "./HeaderComponent.vue";
 import Resume from "./Resume/IndexResume.vue";
-import MovementsComponent from "./MovementsComponent.vue";
+import MovementsComponent from "./Movements/IndexMovements.vue";
+import Action from "./ActionComponent.vue";
 
 export default {
   components: {
@@ -25,6 +39,55 @@ export default {
     HeaderComponent,
     Resume,
     MovementsComponent,
+    Action,
   },
+  data() {
+    return{
+    amount: null,
+    label:null,
+    movements: [{
+                id: 0,
+                title: "Movimiento 1",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 1,
+                title: "Movimiento 2",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 2,
+                title: "Movimiento 3",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 3,
+                title: "Movimiento 4",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 4,
+                title: "Movimiento 5",
+                description: "Lorem ipsum dolor sit amet",
+                amount: -1000,
+            }, {
+                id: 6,
+                title: "Movimiento 7",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 7,
+                title: "Movimiento 8",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 8,
+                title: "Movimiento 9",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }]
+        }
+    },
 };
+
 </script>
--- ahorro-app/src/components/Resume/IndexResume.vue
+++ ahorro-app/src/components/Resume/IndexResume.vue
@@ -1,9 +1,56 @@
 <template>
-  <main>Resume</main>
+  <main>
+    <p>{{ labelVisual }}</p>
+    <h1> {{ amountCurrency}}</h1>
+    <div class="graphic">
+      <slot name="graphic"></slot>
+    </div>
+    <div class="action">
+      <slot name="action"></slot>
+    </div>
+  </main>
 </template>
 
+<script>
 
+const currencyFormat = new Intl.NumberFormat("es-MX",{
+  style: "currency",
+  currency: "MXN",
+});
+
+export default {
+  props: {
+    totalLabel:{
+      type:String
+    },
+    label: {
+      type: String,
+    },
+    totalAmount:{
+      type:Number
+    },
+  amount: {
+    type: Number,
+    default : null,
+  },
+},
+computed: {
+  amountVisual(){
+    return this.amount !== null? this.amount : this.totalAmount;
+  },
+  labelVisual(){
+    return this.label !== null? this.label : this.totalLabel;
+  },
+  amountCurrency(){
+    return  currencyFormat.format(this.amountVisual);
+    
+  }
+}
+};
+
+</script>
 <style scoped>
+
 main {
   display: flex;
   justify-content: center;
@@ -11,18 +58,15 @@ main {
   flex-direction: column;
   width: 100%;
 }
-
 h1,
 p {
   margin: 0;
   text-align: center;
 }
-
 h1 {
   margin-top: 14px;
   color: var(--brand-green);
 }
-
 .graphic {
   display: flex;
   justify-content: center;
@@ -31,4 +75,5 @@ h1 {
   padding: 48px 24px;
   box-sizing: border-box;
 }
+
 </style>
